db98f7f208133359802b79fa2fc60c5d15c2bcbd
Deepak Kumar <74102432+itsmedeepak@users.noreply.github.com>
2026-02-11 23:14:00 +0530

    Update API_URL to new endpoint

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -2,6 +2,9 @@
 // const API_URL = 'http://localhost:4040';
 
 
-const API_URL = 'https://slimy-letitia-itsmedeepak-30b54732.koyeb.app';
+// const API_URL = 'https://slimy-letitia-itsmedeepak-30b54732.koyeb.app';
+const API_URL = 'https://equal-aleda-deepakdns-2b7b3d26.koyeb.app';
+
+
 
 export { API_URL }
